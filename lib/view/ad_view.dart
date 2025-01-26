28ad33ab18003bd9b3340af9f815610a78667ce5
Yusuf Güngör <yusufgngor@gmail.com>
2025-01-26 21:19:48 +0300

    wasm support added

---
+++ lib/view/ad_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdView extends StatelessWidget {
+  const AdView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
